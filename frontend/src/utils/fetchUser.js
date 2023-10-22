98baed5955f272528e771592ee467a33bb697675
danyal <danyallsty04@gmail.com>
2023-10-22 11:11:44 -0700

    fetchuser file created

---
+++ frontend/src/utils/fetchUser.js
@@ -0,0 +1,8 @@
+const fetchUser =()=>{
+    const userInfo = localStorage.getItem('user') !== 'undefined' ?  JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+    return userInfo;
+
+
+}
+
+export default fetchUser;
